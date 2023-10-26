717a6721370554738728cf83901ff1d8a3e74f12
rRodeschMNSU <ryan.rodesch@mnsu.edu>
2023-10-26 15:07:57 -0500

    ref: Changed to decodable format

--- source/EncodePWD.py
+++ source/EncodePWD.py
@@ -1,47 +1,42 @@
-import hashlib
 import random
 
-# This encodes the password via SHA-256 salting
-# https://en.wikipedia.org/wiki/Salt_(cryptography)
+def generate_sub_list():
+    valid_chars = 'abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&'
+    lst = [*valid_chars]
+    random.shuffle(lst)
+    return lst
 
-def generate_salt(length: int):
-    salt = ""
-    for PLACEHOLDER in range(length):
-        is_num = bool(random.randint(0, 1))
-        if is_num:
-            salt += str(random.randint(0, 9))
+def encode_with_sublist(lst, string):
+    lst = [*lst]
+    valid_chars = 'abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&'
+    char_map = {valid_chars[i]: lst[i] for i in range(len(valid_chars))}
+    encoded_string = ''
+    for char in string:
+        if char in char_map:
+            encoded_string += char_map[char]
         else:
-            salt += str(chr(random.randint(0, 25) + ord("a")))
-    return salt
+            encoded_string += char
+    return encoded_string
 
 # Writes password to local passwrod storage
-
 def save_to_local(append):
     file = open("passwords.txt", "a")
-    file.write(append)
-
-# Generates radon salt
-# https://en.wikipedia.org/wiki/Salt_(cryptography)
-
-def hash_it(salt, password):
-    hasher = hashlib.sha256()
-    hasher.update(salt.encode("utf-8"))
-    hasher.update(password.encode("utf-8"))
-    return hasher.hexdigest()
+    file.write(f"{append}\n")
 
 # Class for importing in other methods
 class Encode:
     def __init__(self, site, password):
         self.site = site
         self.password = password
-        self.hashed = ""
+        self.sublist = ""
+        self.encoded = ""
 
     def __call__(self):
-        print(f"Encoding {self.site}'s password and saving to local storage")
-        salt = generate_salt(5)
-        self.hashed = hash_it(salt, self.password)
-        formatted_append = f"{self.site} {self.hashed} {salt}\n"
-        save_to_local(formatted_append)
-
+        self.sublist = "".join(generate_sub_list())
+        self.encoded = encode_with_sublist(self.sublist, self.password)
+        save_to_local(f"{self.site} {self.encoded} {self.sublist}")
     def __str__(self):
-        return f"The hashed password for {self.site} is {self.hashed}"
+        return f"The hashed password for {self.site} is {self.encoded}"
+coded = Encode("youtu.be", "o#211657o")
+coded()
+print(coded)
