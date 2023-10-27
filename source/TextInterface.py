1d9830a690a37aa1a69e7cc9dea2c69884d5d74a
evanjanderson05 <143127525+evanjanderson05@users.noreply.github.com>
2023-10-27 00:00:49 -0500

    added comments

--- source/TextInterface.py
+++ source/TextInterface.py
@@ -1,26 +1,26 @@
-# Prompt for either encode or decode
-# if decode, ask for site
-# if encode, as for site and password
-# on competion, loop the program
+# Import the encode and decoding modules
 from EncodePWD import Encode
 from DecodePWD import Decode
 
+# Encoding function
 def encode_user_in(site: str):
     passcode =  input("Enter desired password to encode: ")
     try:
-        encoder = Encode(site, passcode)
+        encoder = Encode(site, passcode) # Create an Encode object before calling it
         encoder()
-    except:
+    except: # If encoding fails for whatever reason
         "Encoding failed due to an error withn your input, please restart and try again."
         exit()
     print("Password saved successfully.")
 
+# Decoding function
 def decode_user_in(site: str):
-    decoded = Decode(site)
+    decoded = Decode(site) # Create a Decode object before calling it
     decoded()
     pass
-while True:
-    is_decode = bool(int(input("Encode: 1\nDecode: 2\n"))-1)
+
+while True: # Loop program while it is running
+    is_decode = bool(int(input("Encode: 1\nDecode: 2\n"))-1) # Prompt for whether to encode or decode & convert to boolean
     site = input("Enter site URL:")
     if not is_decode:
         encode_user_in(site)
